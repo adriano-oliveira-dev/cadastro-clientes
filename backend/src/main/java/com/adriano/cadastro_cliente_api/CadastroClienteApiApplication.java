8ebd2c8dae51b3e170ac8b44000c3fbc507a1493
Adriano <contatoworkadriano@gmail.com>
2026-06-17 00:52:37 -0300

    fix: remove backend submodule and add as regular folder

---
+++ backend/src/main/java/com/adriano/cadastro_cliente_api/CadastroClienteApiApplication.java
@@ -0,0 +1,13 @@
+package com.adriano.cadastro_cliente_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClienteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClienteApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/adriano/cadastro_cliente_api/CadastroClienteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriano.cadastro_cliente_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClienteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
